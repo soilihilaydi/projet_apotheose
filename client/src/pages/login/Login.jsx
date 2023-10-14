c0584a66c54c264b44f1eed41b2fc63509192496
soilihi laydi <soilihi.laydi@gmail.com>
2023-10-14 17:26:04 +0200

    creation login register whit react router

--- client/src/pages/login/Login.jsx
+++ client/src/pages/login/Login.jsx
@@ -1,9 +1,35 @@
+import { Link } from "react-router-dom";
 import "./login.scss"
 
 export const Login = () => {
   return (
-    <div className="login">Login super</div>
+    <div className="login">
+      <div className="card">
+      <div className="left">
+        <h1>Elektron konnection</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero cum,
+            alias totam numquam ipsa exercitationem dignissimos, error nam,
+            consequatur.
+            </p>
+            <span>Vous n'avez pas de compte?</span>
+            <Link to="/register">
+              <button>Enregistre vous</button>
+            </Link>
+            
+      </div>
+      <div className="right">
+        <h1>Se connecter</h1>
+        <form>
+        <input type="text" placeholder="Nom d'utilisateur"/>
+        <input type="password" placeholder="Mot de passe"/>
+        <button>Se connecter</button>
+
+        </form>
+      </div>
+
+      </div>
+    </div>
   )
 }
 
-export default Login
+export default Login;
--- client/src/pages/register/Register.jsx
+++ client/src/pages/register/Register.jsx
@@ -1,10 +1,37 @@
+import { Link } from "react-router-dom";
 import "./register.scss"
 
 export const Register = () => {
   return (
-    <div className="Register">Register</div>
+    <div className="register">
+      <div className="card">
+      <div className="left">
+        <h1>Elektron konnection</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero cum,
+            alias totam numquam ipsa exercitationem dignissimos, error nam,
+            consequatur.
+            </p>
+            <span>Vous avez un compte?</span>
+            <Link to="/login">
+              <button>Se connecter</button>
+            </Link>
+            
+      </div>
+      <div className="right">
+        <h1>S'enregistrer</h1>
+        <form>
+        <input type="text" placeholder="Nom d'utilisateur" />
+            <input type="email" placeholder="Email" />
+            <input type="password" placeholder="Mot de passe" />
+            <input type="text" placeholder="Nom" />
+        <button>S'inscrire</button>
+
+        </form>
+      </div>
+
+      </div>
+    </div>
   )
 }
 
-
-export default Register
+export default Register;
