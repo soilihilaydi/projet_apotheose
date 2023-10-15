d8f436a528f8c9f36cbda8f07708928736730678
soilihi laydi <soilihi.laydi@gmail.com>
2023-10-15 14:19:19 +0200

    creation de la navbar

--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -1,8 +1,39 @@
 import "./navbar.scss";
+import HomeOutlinedIcon from "@mui/icons-material/HomeOutlined";
+import DarkModeOutlinedIcon from "@mui/icons-material/DarkModeOutlined";
+import WbSunnyOutlinedIcon from "@mui/icons-material/WbSunnyOutlined";
+import GridViewOutlinedIcon from "@mui/icons-material/GridViewOutlined";
+import NotificationsOutlinedIcon from "@mui/icons-material/NotificationsOutlined";
+import EmailOutlinedIcon from "@mui/icons-material/EmailOutlined";
+import PersonOutlinedIcon from "@mui/icons-material/PersonOutlined";
+import SearchOutlinedIcon from "@mui/icons-material/SearchOutlined";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <div className="navbar">Navbar</div>
+    <div className="navbar">
+      <div className="left">
+        <Link to="/" style={{textDecoration:"none"}}>
+        <span>Elektron konnection</span>
+        </Link>
+        <HomeOutlinedIcon/>
+        <DarkModeOutlinedIcon/>
+        <GridViewOutlinedIcon/>
+        <div className="search">
+          <SearchOutlinedIcon/>
+          <input type="text" placeholder="Recherche..."/>
+        </div>
+      </div>
+      <div className="right">
+        <PersonOutlinedIcon/>
+        <EmailOutlinedIcon/>
+        <NotificationsOutlinedIcon/>
+        <div className="user">
+          {/*<img src="https://cdn.www.elektron.se/media/wysiwyg/Syntakt-hero1.jpg" alt="Syntakt 12 track drum computer and synthesizer" />*/}
+          <span>Mickeal jackson</span>
+        </div>
+      </div>
+    </div>
   )
 }
 
