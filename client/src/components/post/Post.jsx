7cf72d852f31c3e5bef0652b20cbdb4befaf91bc
soilihi laydi <soilihi.laydi@gmail.com>
2023-10-17 15:00:00 +0200

    creation dossier et fichier post

---
+++ client/src/components/post/Post.jsx
@@ -0,0 +1,56 @@
+
+import "./post.scss"
+import FavoriteBorderOutlinedIcon from "@mui/icons-material/FavoriteBorderOutlined";
+import FavoriteOutlinedIcon from "@mui/icons-material/FavoriteOutlined";
+import TextsmsOutlinedIcon from "@mui/icons-material/TextsmsOutlined";
+import ShareOutlinedIcon from "@mui/icons-material/ShareOutlined";
+import MoreHorizIcon from "@mui/icons-material/MoreHoriz";
+import { Link } from "@mui/material";
+
+const Post = ({post}) => {
+
+     //TEMPORARY
+  const liked = false;
+
+
+
+  return (
+    <div className="post">
+     <div className="container">
+        <div className="user">
+          <div className="userInfo">
+          <img src={post.profilePic} alt="image du profil" />
+            <div className="details">
+              <Link to={`/profile/${post.userId}`} style={{ textDecoration: "none", color: "inherit" }}>
+              <span>{post.name}</span>
+              <span className="date"> il y a 1 minute</span>
+              </Link>
+            </div>
+          </div>
+          <MoreHorizIcon/>
+        </div>
+        <div className="content">
+        <p>{post.desc}</p>
+          <img src={post.img} alt="image du multi effet d'elektron" />
+        </div>
+        <div className="info">
+          <div className="item">
+          {liked ? <FavoriteOutlinedIcon /> : <FavoriteBorderOutlinedIcon />}
+          12 J'aime
+          </div>
+          <div className="item">
+          <TextsmsOutlinedIcon />
+          12 Commentaires
+          </div>
+          <div className="item">
+          <ShareOutlinedIcon />
+          Partager
+          </div>
+        </div>
+        
+      </div>
+    </div>
+  );
+};
+
+export default Post;
