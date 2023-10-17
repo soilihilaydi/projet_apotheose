b82ae720528741f289e0c8bac3dcc69870470c7a
soilihi laydi <soilihi.laydi@gmail.com>
2023-10-17 18:23:12 +0200

    creation dossier et fichier profile

--- client/src/pages/profile/Profile.jsx
+++ client/src/pages/profile/Profile.jsx
@@ -1,10 +1,73 @@
 import "./profile.scss"
+import FacebookTwoToneIcon from "@mui/icons-material/FacebookTwoTone";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import InstagramIcon from "@mui/icons-material/Instagram";
+import PinterestIcon from "@mui/icons-material/Pinterest";
+import TwitterIcon from "@mui/icons-material/Twitter";
+import PlaceIcon from "@mui/icons-material/Place";
+import LanguageIcon from "@mui/icons-material/Language";
+import EmailOutlinedIcon from "@mui/icons-material/EmailOutlined";
+import MoreVertIcon from "@mui/icons-material/MoreVert";
+import Posts from "../../components/posts/Posts"
 
-export const Profile = () => {
+
+const Profile = () => {
   return (
-    <div className="profile">Profile</div>
-  )
-}
+    <div className="profile">
+      <div className="images">
+        <img
+          src="https://w.forfun.com/fetch/e0/e0a002193fb507a2a270c5cb79de696c.jpeg?h=1200&r=0.5"
+          alt=""
+          className="cover"
+        />
+        <img
+          src="https://upload.wikimedia.org/wikipedia/commons/a/ad/Jackson_5_1974_%28Michael%29.jpg"
+          alt=""
+          className="profilePic"
+        />
+      </div>
+      <div className="profileContainer">
+        <div className="uInfo">
+          <div className="left">
+            <a href="http://facebook.com">
+              <FacebookTwoToneIcon fontSize="large" />
+            </a>
+            <a href="http://facebook.com">
+              <InstagramIcon fontSize="large" />
+            </a>
+            <a href="http://facebook.com">
+              <TwitterIcon fontSize="large" />
+            </a>
+            <a href="http://facebook.com">
+              <LinkedInIcon fontSize="large" />
+            </a>
+            <a href="http://facebook.com">
+              <PinterestIcon fontSize="large" />
+            </a>
+          </div>
+          <div className="center">
+            <span>Mickeal Jackson</span>
+            <div className="info">
+              <div className="item">
+                <PlaceIcon />
+                <span>Ardeche</span>
+              </div>
+              <div className="item">
+                <LanguageIcon />
+                <span>Mickeal Jackson.electronik-Musik</span>
+              </div>
+            </div>
+            <button>Suivre</button>
+          </div>
+          <div className="right">
+            <EmailOutlinedIcon />
+            <MoreVertIcon />
+          </div>
+        </div>
+      <Posts/>
+      </div>
+    </div>
+  );
+};
 
-
-export default Profile
+export default Profile;
