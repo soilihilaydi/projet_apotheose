8f767d77120832b51c636d508ed5faf7ea0cad23
soilihi laydi <soilihi.laydi@gmail.com>
2023-10-17 18:06:41 +0200

    creation dossier et fichier share

--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -1,11 +1,13 @@
 import "./home.scss"
 import Stories from "../../components/stories/Stories"
 import Posts from "../../components/posts/Posts"
+import Share from "../../components/share/Share"
 
 export const Home = () => {
   return (
     <div className="home">
      <Stories/>
+     <Share/>
      <Posts/>
     </div>
   )
