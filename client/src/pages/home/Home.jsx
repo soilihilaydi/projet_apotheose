3aebf65b73f22876f277889bb84a7810ca532019
soilihi laydi <soilihi.laydi@gmail.com>
2023-10-17 13:43:17 +0200

    fix

--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -1,10 +1,12 @@
 import "./home.scss"
 import Stories from "../../components/stories/Stories"
+import Posts from "../../components/posts/Posts"
 
 export const Home = () => {
   return (
     <div className="home">
      <Stories/>
+     <Posts/>
     </div>
   )
 }
