4634413beb7143ed2f8a3118c540b748819f9c34
soilihi laydi <soilihi.laydi@gmail.com>
2023-10-19 20:10:38 +0200

    page post

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,8 @@
-
 import Login from "./pages/login/Login";
-import Register from "./pages/register/Register"
-import { 
+import Register from "./pages/register/Register";
+import {
   createBrowserRouter,
   RouterProvider,
-  Route,
   Outlet,
   Navigate,
 } from "react-router-dom";
@@ -14,30 +12,32 @@ import RightBar from "./components/rightBar/RightBar";
 import Home from "./pages/home/Home";
 import Profile from "./pages/profile/Profile";
 import "./style.scss";
-import React, { useContext } from 'react';
+import { useContext } from "react";
 import { DarkModeContext } from "./context/darkModeContext";
 import { AuthContext } from "./context/authContext";
-
+import { QueryClient, QueryClientProvider } from "react-query";
 
 function App() {
-
-  const {currentUser} = useContext(AuthContext);
+  const { currentUser } = useContext(AuthContext);
 
   const { darkMode } = useContext(DarkModeContext);
 
+  const queryClient = new QueryClient();
 
-  const Layout = ()=>{
-    return(
-      <div className={`theme-${darkMode ? "dark" : "light"}`}>
-        <Navbar/>
-        <div style={{display:"flex"}}>
-          <LeftBar />
-          <div style={{ flex:6 }}>
-            <Outlet />
+  const Layout = () => {
+    return (
+      <QueryClientProvider client={queryClient}>
+        <div className={`theme-${darkMode ? "dark" : "light"}`}>
+          <Navbar />
+          <div style={{ display: "flex" }}>
+            <LeftBar />
+            <div style={{ flex: 6 }}>
+              <Outlet />
+            </div>
+            <RightBar />
           </div>
-          <RightBar />
         </div>
-      </div>
+      </QueryClientProvider>
     );
   };
 
@@ -51,36 +51,37 @@ function App() {
 
   const router = createBrowserRouter([
     {
-      path:"/",
-      element:<ProtectedRoute>
-              <Layout/>
-              </ProtectedRoute>,
-              
-      children:[
+      path: "/",
+      element: (
+        <ProtectedRoute>
+          <Layout />
+        </ProtectedRoute>
+      ),
+      children: [
         {
-          path:"/",
-          element:<Home/>,
+          path: "/",
+          element: <Home />,
         },
         {
-          path:"/profile/:id",
-          element:<Profile/>,
+          path: "/profile/:id",
+          element: <Profile />,
         },
       ],
     },
     {
       path: "/login",
-      element: <Login/>,
+      element: <Login />,
     },
     {
       path: "/register",
-      element: <Register/>,
+      element: <Register />,
     },
   ]);
 
   return (
-  <div>
-   <RouterProvider router={router} />
-  </div>
+    <div>
+      <RouterProvider router={router} />
+    </div>
   );
 }
 
--- client/src/components/share/Share.jsx
+++ client/src/components/share/Share.jsx
@@ -2,26 +2,78 @@ import "./share.scss";
 import Image from "../../assets/img.png";
 import Map from "../../assets/map.png";
 import Friend from "../../assets/friend.png";
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { AuthContext } from "../../context/authContext";
-
+import { useMutation, useQueryClient } from "react-query";
+import { makeRequest } from "../../axios";
 const Share = () => {
+  const [file, setFile] = useState(null);
+  const [desc, setDesc] = useState("");
 
-  const {currentUser} = useContext(AuthContext)
+  const upload = async () => {
+    try {
+      const formData = new FormData();
+      formData.append("file", file);
+      const res = await makeRequest.post("/upload", formData);
+      return res.data;
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const { currentUser } = useContext(AuthContext);
+
+  const queryClient = useQueryClient();
+
+  const mutation = useMutation(
+    (newPost) => {
+      return makeRequest.post("/posts", newPost);
+    },
+    {
+      onSuccess: () => {
+        // Invalidate and refetch
+        queryClient.invalidateQueries(["posts"]);
+      },
+    }
+  );
+
+  const handleClick = async (e) => {
+    e.preventDefault();
+    let imgUrl = "";
+    if (file) imgUrl = await upload();
+    mutation.mutate({ desc, img: imgUrl });
+    setDesc("");
+    setFile(null);
+  };
+
   return (
     <div className="share">
       <div className="container">
         <div className="top">
-          <img
-            src={currentUser.profilePic}
-            alt=""
-          />
-          <input type="text" placeholder={`Quel sujet occupe tes pensées en ce moment ${currentUser.name}?`} />
+          <div className="left">
+            <img src={"/upload/" + currentUser.profilePic} alt="" />
+            <input
+              type="text"
+              placeholder={`Quel sujet occupe tes pensées en ce moment ${currentUser.name}?`}
+              onChange={(e) => setDesc(e.target.value)}
+              value={desc}
+            />
+          </div>
+          <div className="right">
+            {file && (
+              <img className="file" alt="" src={URL.createObjectURL(file)} />
+            )}
+          </div>
         </div>
         <hr />
         <div className="bottom">
           <div className="left">
-            <input type="file" id="file" style={{display:"none"}} />
+            <input
+              type="file"
+              id="file"
+              style={{ display: "none" }}
+              onChange={(e) => setFile(e.target.files[0])}
+            />
             <label htmlFor="file">
               <div className="item">
                 <img src={Image} alt="" />
@@ -38,7 +90,7 @@ const Share = () => {
             </div>
           </div>
           <div className="right">
-            <button>Partager</button>
+            <button onClick={handleClick}>Partager</button>
           </div>
         </div>
       </div>
