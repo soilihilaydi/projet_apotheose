ef9a5cbc6d897cf9b65f679b8af859edd7bdbfe8
soilihi laydi <soilihi.laydi@gmail.com>
2023-10-19 17:42:16 +0200

    page register et login

--- client/src/pages/login/Login.jsx
+++ client/src/pages/login/Login.jsx
@@ -1,44 +1,69 @@
-import { useContext } from "react";
-import { Link } from "react-router-dom";
+import { useContext, useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 import { AuthContext } from "../../context/authContext";
-import "./login.scss"
+import "./login.scss";
 
-export const Login = () => {
+const Login = () => {
+  const [inputs, setInputs] = useState({
+    username: "",
+    password: "",
+  });
+  const [err, setErr] = useState(null);
 
+  const navigate = useNavigate()
+
+  const handleChange = (e) => {
+    setInputs((prev) => ({ ...prev, [e.target.name]: e.target.value }));
+  };
   const { login } = useContext(AuthContext);
 
-  const handleLogin = () => { login();};
-    
-  
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    try {
+      await login(inputs);
+      navigate("/")
+    } catch (err) {
+      setErr(err.response.data);
+    }
+  };
 
   return (
     <div className="login">
       <div className="card">
-      <div className="left">
-        <h1>Elektron konnection</h1>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero cum,
+        <div className="left">
+          <h1>Elektron konnection.</h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero cum,
             alias totam numquam ipsa exercitationem dignissimos, error nam,
             consequatur.
-            </p>
-            <span>Vous n'avez pas de compte?</span>
-            <Link to="/register">
-              <button>Enregistre vous</button>
-            </Link>
-            
-      </div>
-      <div className="right">
-        <h1>Se connecter</h1>
-        <form>
-        <input type="text" placeholder="Nom d'utilisateur"/>
-        <input type="password" placeholder="Mot de passe"/>
-        <button onClick={handleLogin}>Login</button>
-
-        </form>
-      </div>
-
+          </p>
+          <span>Vous n'avez pas de compte?</span>
+          <Link to="/register">
+            <button>Enregistre vous</button>
+          </Link>
+        </div>
+        <div className="right">
+          <h1>Se connecter</h1>
+          <form>
+            <input
+              type="text"
+              placeholder="Nom d'utilisateur"
+              name="username"
+              onChange={handleChange}
+            />
+            <input
+              type="password"
+              placeholder="Mot de passe"
+              name="password"
+              onChange={handleChange}
+            />
+            {err && err}
+            <button onClick={handleLogin}>Se connecter</button>
+          </form>
+        </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default Login;
--- client/src/pages/register/Register.jsx
+++ client/src/pages/register/Register.jsx
@@ -1,37 +1,82 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
-import "./register.scss"
+import "./register.scss";
+import axios from "axios";
 
-export const Register = () => {
+const Register = () => {
+  const [inputs, setInputs] = useState({
+    username: "",
+    email: "",
+    password: "",
+    name: "",
+  });
+  const [err, setErr] = useState(null);
+
+  const handleChange = (e) => {
+    setInputs((prev) => ({ ...prev, [e.target.name]: e.target.value }));
+  };
+
+  const handleClick = async (e) => {
+    e.preventDefault();
+
+    try {
+      await axios.post("http://localhost:8800/api/auth/register", inputs);
+    } catch (err) {
+      setErr(err.response.data);
+    }
+  };
+
+  console.log(err)
+
   return (
     <div className="register">
       <div className="card">
-      <div className="left">
-        <h1>Elektron konnection</h1>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero cum,
+        <div className="left">
+          <h1>Elektron konnection.</h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero cum,
             alias totam numquam ipsa exercitationem dignissimos, error nam,
             consequatur.
-            </p>
-            <span>Vous avez un compte?</span>
-            <Link to="/login">
-              <button>Se connecter</button>
-            </Link>
-            
-      </div>
-      <div className="right">
-        <h1>S'enregistrer</h1>
-        <form>
-        <input type="text" placeholder="Nom d'utilisateur" />
-            <input type="email" placeholder="Email" />
-            <input type="password" placeholder="Mot de passe" />
-            <input type="text" placeholder="Nom" />
-        <button>S'inscrire</button>
-
-        </form>
-      </div>
-
+          </p>
+          <span>Vous avez un compte?</span>
+          <Link to="/login">
+            <button>Se connecter</button>
+          </Link>
+        </div>
+        <div className="right">
+          <h1>S'enregistrer</h1>
+          <form>
+            <input
+              type="text"
+              placeholder="Nom d'utilisateur"
+              name="username"
+              onChange={handleChange}
+            />
+            <input
+              type="email"
+              placeholder="Email"
+              name="email"
+              onChange={handleChange}
+            />
+            <input
+              type="password"
+              placeholder="Mot de passe"
+              name="password"
+              onChange={handleChange}
+            />
+            <input
+              type="text"
+              placeholder="Nom"
+              name="name"
+              onChange={handleChange}
+            />
+            {err && err}
+            <button onClick={handleClick}>S'inscrire</button>
+          </form>
+        </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default Register;
