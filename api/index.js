4c2467926ec09f8ec1421701f1d0e096a43ff3e6
soilihi laydi <soilihi.laydi@gmail.com>
2023-10-21 21:47:29 +0200

    probleme datbase

--- api/index.js
+++ api/index.js
@@ -1,26 +1,27 @@
-import  Express  from "express";
-const app = Express()
+import express from "express";
+const app = express();
 import authRoutes from "./routes/auth.js";
 import userRoutes from "./routes/users.js";
 import postRoutes from "./routes/posts.js";
 import commentRoutes from "./routes/comments.js";
 import likeRoutes from "./routes/likes.js";
+import relationshipRoutes from "./routes/relationships.js";
 import cors from "cors";
-import cookieParser from "cookie-parser";
 import multer from "multer";
+import cookieParser from "cookie-parser";
 
 //middlewares
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Credentials", true);
   next();
 });
-app.use(Express.json())
+app.use(express.json());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "http://localhost:3000",
   })
 );
-app.use(cookieParser())
+app.use(cookieParser());
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
@@ -38,14 +39,13 @@ app.post("/api/upload", upload.single("file"), (req, res) => {
   res.status(200).json(file.filename);
 });
 
-
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/posts", postRoutes);
 app.use("/api/comments", commentRoutes);
 app.use("/api/likes", likeRoutes);
+app.use("/api/relationships", relationshipRoutes);
 
-
-app.listen(8800, ()=>{
+app.listen(8800, () => {
   console.log("API working!");
-})
+});
--- api/routes/users.js
+++ api/routes/users.js
@@ -1,9 +1,10 @@
-import  Express  from "express";
-import { getUser } from "../controllers/users.js";
+import express from "express";
+import { getUser , updateUser} from "../controllers/user.js";
 
-const router = Express.Router()
+const router = express.Router()
 
 router.get("/find/:userId", getUser)
+router.put("/", updateUser)
 
 
 export default router
